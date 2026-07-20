8f508cd672ee1c1c8ecfab7f25d38e3742671b06
Suman2205 <suman27574@gmail.com>
2026-07-21 00:12:33 +0530

    Implement anchor tag traversal and URL extraction from href attributes

--- src/LinkExtractor.cpp
+++ src/LinkExtractor.cpp
@@ -42,3 +42,98 @@ bool LinkExtractor::isAnchorTag(const string& html,int index){
     char next = html[index + 2];
     return isWhiteSpace(next) || next == '>';
 }
+
+
+DynamicArray<string> LinkExtractor::extractLinks(const string& html){
+    DynamicArray<string> links;
+    int i = 0;
+    while(i < html.length()){
+        //--------------------------------------------------
+        // Look for opening <a tag
+        //--------------------------------------------------
+        if(isAnchorTag(html, i)){
+            i += 2;
+            while(i < html.length() && html[i] != '>'){
+                skipWhiteSpace(html, i);
+                //--------------------------------------------------
+                // Check for href attribute
+                //--------------------------------------------------
+                if(startsWith(html, i, "href")){
+                    int end = i + 4;
+                    // href must end with whitespace, = or >
+                    if(end < html.length() &&!isWhiteSpace(html[end]) &&html[end] != '=' && html[end] != '>'){
+                        i++;
+                        continue;
+                    }
+                    i += 4;
+                    skipWhiteSpace(html, i);
+                    if(i >= html.length() || html[i] != '='){
+                        i++;
+                        continue;
+                    }
+                    i++;
+                    skipWhiteSpace(html, i);
+                    if(i >= html.length()){
+                        break;
+                    }
+                    string url;
+                    //--------------------------------------------------
+                    // Double quoted value
+                    //--------------------------------------------------
+
+                    if(html[i] == '"'){
+                        i++;
+                        while(i < html.length() && html[i] != '"'){
+                            url += html[i];
+                            i++;
+                        }
+                        if(i < html.length()){
+                            i++;
+                        }
+                    }
+
+                    //--------------------------------------------------
+                    // Single quoted value
+                    //--------------------------------------------------
+
+                    else if(html[i] == '\''){
+                        i++;
+                        while(i < html.length() && html[i] != '\''){
+                            url += html[i];
+                            i++;
+                        }
+                        if(i < html.length()){
+                            i++;
+                        }
+                    }
+                    //--------------------------------------------------
+                    // Unquoted value
+                    //--------------------------------------------------
+
+                    else
+                    {
+                        while(i < html.length() && !isWhiteSpace(html[i]) && html[i] != '>')
+                        {
+                            url += html[i];
+                            i++;
+                        }
+                    }
+                    if(!url.empty()){
+                        links.push_back(url);
+                    }
+                }
+                //--------------------------------------------------
+                // Skip current attribute
+                //--------------------------------------------------
+
+                else{
+                    while(i < html.length() && !isWhiteSpace(html[i]) && html[i] != '>'){
+                        i++;
+                    }
+                }
+            }
+        }
+        i++;
+    }
+    return links;
+}
